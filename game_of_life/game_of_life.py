a6bde99cfa14bfc184915d9761ff92ffb3797abc
Priscilla Baah <pyfbaah@gmail.com>
2022-02-02 11:13:47 +0000

    generalize for edge cases

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,6 +1,5 @@
 # milestone 1: store the board state
 import random
-from turtle import width
 def random_state(width:int, height:int) -> list[list]:
     """
     Creates a two dimensional array based on given width and height values.
@@ -30,17 +29,52 @@ def render(state):
         print("|", row, "|")
     print("---"*len(state))
     
-state = random_state(20, 20)
-# render(state)
+
+
 
 # milestone 3: next_board_state
 def sum_neighbors(cell_i, board):
     i = cell_i[0]
     j = cell_i[1]
+    n_i = len(board)- 1
+    n_j = len(board[0]) - 1
     neighbors = []
-    neighbors.extend(board[i-1][j-1:j+2]) # first row of neighboring cells
-    neighbors.extend(board[i+0][j-1:j+2]) # second, this includes active cell
-    neighbors.extend(board[i+1][j-1:j+2]) # third
+    if cell_i == [0, 0] or cell_i == [0, n_j] or cell_i == [n_i, 0] or cell_i == [n_i, n_j]:
+        return -1
+
+    if i == 0:
+        neighbors.extend(board[i+n_i][j-1:j+2])
+        neighbors.extend(board[i+0][j-1:j+2])
+        neighbors.extend(board[i+1][j-1:j+2])
+        
+        
+    
+    elif i == n_i:
+        neighbors.extend(board[i+-1][j-1:j+2])
+        neighbors.extend(board[i+0][j-1:j+2])
+        neighbors.extend(board[i-n_i][j-1:j+2])
+    
+    elif j == 0:
+        neighbors.append(board[i-1][n_j])
+        neighbors.extend(board[i-1][j:j+2])
+        neighbors.append(board[i+0][n_j])
+        neighbors.extend(board[i+0][j:j+2])
+        neighbors.append(board[i+1][n_j])
+        neighbors.extend(board[i+1][j-1:j+2])
+
+    elif j == n_j:
+        neighbors.extend(board[i-1][j-1:j+1])
+        neighbors.append(board[i-1][n_j-n_j])
+        neighbors.extend(board[i+0][j-1:j+1])
+        neighbors.append(board[i+0][n_j-n_j])
+        neighbors.extend(board[i+1][j-1:j+1])
+        neighbors.append(board[i+1][n_j-n_j])
+
+    else:
+        neighbors.extend(board[i-1][j-1:j+2]) # first row of neighboring cells
+        neighbors.extend(board[i+0][j-1:j+2]) # second, this includes active cell
+        neighbors.extend(board[i+1][j-1:j+2]) # third
+
     del neighbors[4] # removes the active cell, which will always be the fifth element
     return sum(neighbors)
 
@@ -51,22 +85,25 @@ def next_board_state(init_state):
     if len(init_state) * len(init_state[0]) < 9:
         raise Exception("length and height of initial stage is too small to compute")
     # first take a cell
-    for i in range(1, len(init_state)-1):
-        for j in range(1, len(init_state[i])-1):
-            current_i = [i, j]
-            live_neighbors = sum_neighbors(current_i, init_state)
+    for i in range(len(init_state)):
+        for j in range(len(init_state[i])):
+            live_neighbors = sum_neighbors([i, j], init_state)
 
-            if live_neighbors <= 1:
+            if live_neighbors < 0:
+                init_state[i][j] =  init_state[i][j] #init state remains unchanged for corner cells
+            elif live_neighbors <= 1:
                 init_state[i][j] = 0
             elif live_neighbors <= 3:
                 init_state[i][j] = 1
             else:
                 init_state[i][j] = 0
     return init_state
-render(state)             
+
+state = random_state(20, 20)
+render(state)  
+print("****************************************************")           
 board_state = next_board_state(state)
 render(board_state)
-    # get its next 8 neighbors 
-    # sum their values
-    # update the state of the cell based on the sum of neighbor values
+# print(state)
+    
 
